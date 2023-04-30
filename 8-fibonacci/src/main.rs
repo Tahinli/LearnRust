bb9a14c6e8516621bf2d2c6e4b9c0446993fc1aa
Tahinli <>
2023-04-30 20:35:55 +0300

    fibonacci_calculation

---
+++ 8-fibonacci/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+fn get_input()->u32
+    {
+        loop
+            {
+                let mut input = String::new();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Failed to read line");
+                match input.trim().parse()
+                    {
+                        Ok(num) => return num,
+                        Err(_) =>
+                            {
+                                println!("Expected Valid Number");
+                            }
+                    };
+                }
+    }
+fn fibo(val:u32)->u128
+    {
+        let mut a = 0;
+        let mut b = 1;
+        let mut c;
+        for _element in 2..=val
+            {
+                c = a+b;
+                a = b;
+                b = c;
+            }
+        return b;
+    }
+fn main() 
+    {
+        println!("Hello, world!");
+        println!("Give a Number for Fibonacci Calculation ↓");
+        println!("Result = {}",fibo(get_input()));
+    }
