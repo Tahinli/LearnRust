b59e09d1761bf64f8e0387e7e6e9c267532f00f8
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2025-09-07 11:03:11 +0300

    commented better

--- 11-ownership/src/main.rs
+++ 11-ownership/src/main.rs
@@ -4,7 +4,7 @@ fn main()
         
         let n = 5;
         let s = String::from("Learning Ownership");
-        take_ownership(s, n);
+        take_ownership(s, n);//s is moved n is copied
         //println!("{}", s); It's gone
         println!("Not Transferred, Just Copied = {}", n);
     
@@ -15,4 +15,4 @@ fn take_ownership(our_string:String, our_number:i32)
         //ownership is transferred for string
         println!("I got your String = {}", our_string);
         println!("I got your Number = {}", our_number);
-    }   //calls drop for string, so our_string is gone now
+    }   //calls "drop" for string, so our_string is gone now
